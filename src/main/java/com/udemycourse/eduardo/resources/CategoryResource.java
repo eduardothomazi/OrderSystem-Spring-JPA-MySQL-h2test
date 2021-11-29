336e72e9c6d75ec59b9b6feb8015554ad8bbb678
Eduardo Thomazi <eduardothomazi91@gmail.com>
2021-11-29 14:56:09 -0300

    C1 - Implementing H2 database, entities package and endpoint test

--- src/main/java/com/udemycourse/eduardo/resources/CategoryResource.java
+++ src/main/java/com/udemycourse/eduardo/resources/CategoryResource.java
@@ -1,15 +1,24 @@
 package com.udemycourse.eduardo.resources;
 
+import com.udemycourse.eduardo.entities.Category;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String test(){
-        return "Test ok!";
+    public List<Category> test(){
+        Category cat1 = new Category(1L, "Informatica");
+        Category cat2 = new Category(2L, "Escritorio");
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.addAll(Arrays.asList(cat1, cat2));
+        return categoryList;
     }
 }
