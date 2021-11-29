fd14af4e472da4687bf5d45e8f7e7732af4710ea
Eduardo Thomazi <eduardothomazi91@gmail.com>
2021-11-29 14:33:57 -0300

    C0 - Creating project and testing first endpoint

---
+++ src/main/java/com/udemycourse/eduardo/resources/CategoryResource.java
@@ -0,0 +1,15 @@
+package com.udemycourse.eduardo.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String test(){
+        return "Test ok!";
+    }
+}
---
+++ src/test/java/com/udemycourse/eduardo/EduardoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemycourse.eduardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduardoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
